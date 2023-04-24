ff09ba00ac7ad878466712e340a0c5738704157b
arlauwer <Arno.Lauwers@UGent.be>
2023-04-24 12:05:23 +0200

    S instead of sigma in hamiltonian calc_groundstate in main

--- hamiltonian.jl
+++ hamiltonian.jl
@@ -6,11 +6,8 @@ export AKLT_hamiltonian
 using MPSKitModels, MPSKit, TensorKit, TensorOperations
 
 function AKLT_hamiltonian(eltype=ComplexF64, symmetry=ℤ{1}, lattice=InfiniteChain(1); J=1.0, θ=0.0, spin=1)
-    XX = sigma_xx(eltype, symmetry; spin=spin)
-    YY = sigma_yy(eltype, symmetry; spin=spin)
-    ZZ = sigma_zz(eltype, symmetry; spin=spin)
-    return @mpoham sum(J * (cos(θ) * (XX{i,j} + YY{i,j} + ZZ{i,j}) + sin(θ) * (XX{i,j} * XX{i,j} + YY{i,j} * YY{i,j} + ZZ{i,j} * ZZ{i,j}))
-                       for (i, j) in nearest_neighbours(lattice))
+    SS = sigma_exchange(eltype, symmetry; spin=spin)
+    return @mpoham sum(J * (cos(θ) * SS{i,j} + sin(θ) * SS{i,j} * SS{i,j}) for (i, j) in nearest_neighbours(lattice))
 end
 
 end
