f66bfe59764464aa8beb5aac876331f290361b11
arlauwer <Arno.Lauwers@UGent.be>
2023-05-16 12:43:17 +0200

    RAM test notebook text small changes

--- main.jl
+++ main.jl
@@ -4,16 +4,43 @@ include("hamiltonian.jl")
 include("groundstate.jl")
 include("plot_spectrum.jl")
 
-using Logging
-disable_logging(LogLevel(Info))
+# using Logging
+# disable_logging(LogLevel(Info))
 
-H = bilinear_biquadratic_hamiltonian_perturbed(spin=1, θ=0.0, J=1.0, g=0.1)
-Ψ = optimize_groundstate(H, bond=12, maxiter=100)
-spectrum = entanglement_spectrum(Ψ)
-plot_entanglement(spectrum)
 
+"
+Don't keep MPS in memory
+"
+θ_range = linrange(0, π/4, 20, endpoint=false)
+corr = []
+for θ in θ_range
+    H = bilinear_biquadratic_hamiltonian(spin=1, θ=θ)
+    Ψ = optimize_groundstate(H, bond=24, maxiter=500) # good bond dimension for convergence
+    push!(corr, correlation_length(Ψ))
+end
+plot_correlations(θ_range, corr)
+
+
+"
+fixed θ
+varying bond
+"
+# bonds = 12:24
+# correlations = []
+# for D in bonds
+#     println(D)
+#     H = bilinear_biquadratic_hamiltonian(spin=1, θ=π/4)
+#     Ψ = optimize_groundstate(H, bond=D, maxiter=500)
+#     push!(correlations, correlation_length(Ψ))
+# end
+# scatter(bonds, correlations, yscale=:log10, xlabel="bond", ylabel="correlation lengths", legend=false, color=:black)
+
+
+"
+Keep MPS in memory
+"
 # θ_Nematic_approach = linrange(0, π/4, 20; endpoint=false)
-# Ψ_Nematic_list = bilinear_biquadratic_θ_range(θ_Nematic_approach, spin=1, bond=12, maxiter=500)
+# Ψ_Nematic_list = bilinear_biquadratic_θ_range(θ_Nematic_approach, spin=1, bond=20, maxiter=500)
 # correlations_Nematic = correlation_lengths(Ψ_Nematic_list)
 # plot_correlations(θ_Nematic_approach, correlations_Nematic)
 
